918e35aaeafd09ce0100500d8fd62c87206e7902
Anand-Stark <starkyam32@gmail.com>
2024-02-15 00:55:26 +0530

    changes

--- client/src/conatiner/About.jsx
+++ client/src/conatiner/About.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Header } from '../Components'
 import AboutUs from '../Components/AboutUs'
+import {Cart} from '../Components'
+import { useSelector } from 'react-redux'
 
 const About = () => {
+  const isCart = useSelector((state) => state.isCart)
   return (
     <div >
       <Header />
       <AboutUs/>
+      {isCart && <Cart />}
     </div>
   )
 }
--- client/src/conatiner/Contacts.jsx
+++ client/src/conatiner/Contacts.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Header } from '../Components'
 import Contact from '../Components/Contact'
+import {Cart} from '../Components'
+import { useSelector } from 'react-redux'
 
 const Contacts = () => {
+
+  const isCart = useSelector((state) => state.isCart)
+
   return (
     <div className='w-full'>
       <Header />
       <Contact/>
+      {isCart && <Cart />}
     </div>
   )
 }
