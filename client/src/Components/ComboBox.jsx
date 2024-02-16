cdb88dcc125f4c4d8349c2a032fb8dca7676d022
Zeeshan251 <zeeshan202125@gmail.com>
2024-02-16 23:06:25 +0530

    middlewares addition

--- client/src/Components/ComboBox.jsx
+++ client/src/Components/ComboBox.jsx
@@ -25,7 +25,6 @@ const ComboBox = () => {
     return null; // Or you can render a loading indicator or a message
   }
 
-
   const filteredFoodItems = items.filter((item) =>
     item.prod_name.toLowerCase().includes(searchItem.toLowerCase())
   );
@@ -66,10 +65,7 @@ const ComboBox = () => {
 
   return (
     <div>
-      <div
-        className="px-10 py-1 relative font-bold"
-        onClick={handleInputBlur}
-      >
+      <div className="px-10 py-1 relative font-bold" onClick={handleInputBlur}>
         <input
           type="text"
           autoCorrect=""
@@ -80,24 +76,29 @@ const ComboBox = () => {
         />
         {showDropdown && (
           <ul className="absolute top-full left-4 w-[90%] bg-white border border-gray-300 rounded-lg shadow-lg">
-            {filteredFoodItems.map((item, index) => (
-              <>
-
-              <li
-                key={index}
-                className="px-2 py-2 cursor-pointer hover:bg-gray-100 flex justify-evenly rounded-lg items-center gap-4"
-                onClick={() => handleItemClick(item)}
-              >
-                <img className="w-10" src={item.prod_image} alt="Food" />
-
-                <p className="text-md font-bold">{item.prod_name}</p>
-
-                <AddToCart data={item} index={index}/>
-
-              </li>
-              </>
-            ))}
-          </ul>
+          {filteredFoodItems.map((item, index) => (
+            <li
+              key={index}
+              className="px-2 py-2 cursor-pointer hover:bg-gray-100 flex items-center gap-4 relative shadow-md transition duration-300 ease-in-out"
+              onClick={() => handleItemClick(item)}
+            >
+              <div className="w-full flex items-center gap-4 justify-around">
+                <img
+                  className="w-12 h-12 object-cover"
+                  src={item.prod_image}
+                  alt="Food"
+                />
+                
+                  <p className="text-md font-bold">{item.prod_name}</p>
+                  <AddToCart data={item} index={index} />
+                
+              </div>
+              <div className="absolute bottom-0 left-0 right-0 border-b border-gray-300"></div>
+            </li>
+          ))}
+        </ul>
+        
+        
         )}
       </div>
     </div>
--- server/functions/index.js
+++ server/functions/index.js
@@ -4,10 +4,33 @@ require("dotenv").config();
 
 const serviceAccountKey = require("./serviceAccountKey.json");
 const express = require("express");
-
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const rfs = require('rotating-file-stream');
 const app = express();
 
 app.use(express.json());
+
+const logsDirectory = path.join(__dirname, 'logs');
+fs.existsSync(logsDirectory) || fs.mkdirSync(logsDirectory);
+
+// Create a rotating write stream for logging
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d', // rotate every day
+  path: logsDirectory
+});
+
+// Set up Morgan middleware to log requests to the console and to a file
+app.use(morgan('combined', { stream: accessLogStream }));
+
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log("From log middleware");
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 
 // cors origin :
 const cors = require("cors");
@@ -33,7 +56,12 @@ const productRoutes = require("./routes/product")
 app.use('/api/user',userRoutes)
 app.use('/api/products',productRoutes)
 
+
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error("From Error Middleware" , err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
 exports.app = functions.https.onRequest(app)
-
-
-
