b5779eb976119fcd7fa84c76ba7a5a6cb845d0db
Anand-Stark <starkyam32@gmail.com>
2024-04-30 19:52:29 +0530

    added for ci testing 2

--- server/functions/server.js
+++ server/functions/server.js
@@ -2,4 +2,6 @@ const functions = require("firebase-functions");
 const  {app } = require("./index.js");
 
 exports.app = functions.https.onRequest(app);
-//writeen for jkandfj
+//writeen for jkandfjsadfasd
+
+// /sadfsdf
